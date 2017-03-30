8c26bd8dcee95a9c13fcdf6c8aea7ade8bbf00e2
kennynaoh <kennynaoh@gmail.com>
2017-03-30 19:55:43 +0800

    add isPalindrome.c
    use array to handle, need extra space

---
+++ practice-c/isPalindrome.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool isPalindrome(int x){
+	if(x<0) return false;
+	int temp = x;
+	int i = 0;
+	while(temp != 0){
+		temp /= 10;
+		i++;
+	}
+	int *tmp = (int *)malloc(sizeof(int) * i);
+	for(int j=i-1;j>-1;j--){
+		tmp[j] = x%10;
+		x /= 10;
+	}
+
+	int flag = 1;
+	for(int j=0;j<i/2+1;j++){
+		if(tmp[j] != tmp[i-1]){
+			flag = 0;
+			break;
+		}
+		i--;
+	}
+	if(flag == 1) return true;
+	else return false;
+}
+
+int main(){
+	int input;
+	while(1){
+		scanf("%d", &input);
+		printf("%d\n", input);
+		if(isPalindrome(input)) {
+			printf("true\n");
+		}
+	}
+}
