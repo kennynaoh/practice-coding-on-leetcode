aae1abef424b01eded889d34781cfe4901175739
naoh <naoh.cs03g@nctu.edu.tw>
2017-04-11 16:04:45 +0800

    add remove_duplicates in C (#3)
    return the value how many numbers are different

---
+++ practice-c/remove_duplicates.c
@@ -0,0 +1,45 @@
+// Given a sorted array, 
+// remove the duplicates in place such that each element 
+// appear only once and return the new length.
+
+// Do not allocate extra space for another array, you must do this in place with constant memory.
+
+// For example,
+// Given input array nums = [1,1,2],
+
+// Your function should return length = 2, 
+// with the first two elements of nums being 1 and 2 respectively. 
+// It doesn't matter what you leave beyond the new length.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int removeDuplicates(int* nums, int numSize){
+	int count = 1;
+	if(numSize == 1) return count;
+	//if(numSize == 0) return NULL;
+	for(int i=0;i<numSize-1;i++){
+		if(nums[i] != nums[i+1]){
+			count++;
+		}
+	}
+	return count;
+}
+
+int main(){
+	int numSize;
+	printf("how many numbers:\n");
+	scanf("%d", &numSize);
+
+	int *numArray = (int *)malloc(sizeof(int) * numSize);
+	printf("what numbers: \n");
+	for(int i=0;i<numSize;i++){
+		scanf("%d", &numArray[i]);
+	}
+
+	// for (int i = 0; i < numSize; i++) {
+	// 	printf("%d\n", numArray[i]);
+	// }
+
+	printf("%d\n", removeDuplicates(numArray, numSize));
+}
